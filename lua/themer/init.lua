74d93bc954a0e6abd2c3b154f61675d7fac1b98c
MrSandman <mrsandman.h4sh@gmail.com>
2023-07-01 14:02:17 +0200

    feat: just learned to export user commands :O

--- lua/themer/init.lua
+++ lua/themer/init.lua
@@ -1,13 +1,80 @@
 -- COLORIZER
-local telescope = require "telescope"
-local actions = telescope.actions
--- local action_state = telescope.actions.state
-local pickers = telescope.pickers
-local finders = telescope.finders
-local sorters = telescope.sorters
-local themer = require("themer.base")
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local sorters = require "telescope.sorters"
 
-function Colorizer()
-	themer.colors:find()
+function enter(prompt_bufnr)
+	local selected = action_state.get_selected_entry()
+	-- print(vim.inspect(selected))
+	local cmd = 'colorscheme ' .. selected[1]
+	vim.cmd(cmd)
+	actions.close(prompt_bufnr)
 end
 
+function next_color(prompt_bufnr)
+	actions.move_selection_next(prompt_bufnr)
+	local selected = action_state.get_selected_entry()
+	local cmd = 'colorscheme ' .. selected[1]
+	vim.cmd(cmd)
+end
+
+function prev_color(prompt_bufnr)
+	actions.move_selection_previous(prompt_bufnr)
+	local selected = action_state.get_selected_entry()
+	local cmd = 'colorscheme ' .. selected[1]
+	vim.cmd(cmd)
+end
+
+local opts = {
+	-- TODO: Optimise the sorting and make it more responsive
+	prompt_title = "Which color?",
+	layout_strategy = "vertical",
+	layout_config = {
+		height = 0.45,
+		width = 0.25,
+		prompt_position = "top",
+	},
+	sorting_strategy = "ascending",
+	finder = finders.new_table {
+		"rose-pine",
+		"catppuccin",
+		"onedark_dark",
+		"tokyonight",
+		"oxocarbon",
+		"vscode",
+		"doom-one",
+		"github_dark_high_contrast",
+		"tokyodark",
+		"carbonfox",
+		"material",
+		"gruber-darker",
+		"fluoromachine",
+		"nord",
+		"nordic",
+		"dracula",
+		"onenord",
+		"nightfox",
+		"nordfox",
+		"neon",
+		"tokyonight-moon"
+	},
+	sorter = sorters.get_generic_fuzzy_sorter({}),
+	attach_mappings = function(prompt_bufnr, map)
+		map("i", "<CR>", enter)
+		map("i", "<C-k>", prev_color)
+		map("i", "<C-j>", next_color)
+		return true
+	end,
+}
+
+local colors = pickers.new(opts)
+
+-- function colorizer()
+-- 	colors:find()
+-- end
+
+vim.api.nvim_create_user_command("ThemerTest", function()
+	colors:find()
+end, {})
